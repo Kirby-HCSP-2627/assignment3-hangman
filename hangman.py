9562c0cedc579110ffedba9cf33d835a38cfc354
Jose Gomez De La Torre <jgomez@kirby.org>
2026-09-24 08:05:32 -0700

    Implement helper functions for hangman game
    Added helper function stubs for word guessing game.

--- hangman.py
+++ hangman.py
@@ -1,31 +1,21 @@
-# The # symbol is the comment symbol for python, meaning anything after it won't be treated as code.
 # Type your names here: 
 
-# Problem 1
-# Define a function called concert_calculator() that takes 2 required paramaters ticket price and number of tickets and 2 optional parameters sale tax and processing fee
-# Returns a string
-# Example calls of concert_calculator()
-# concert_calculator(100, 3, 0.1, 5.50) -> "The price for 3 tickets at $100 each with 10% sales tax and $5.50 processing fee is $330.5"
-# Insert your code below! 
+# Helper Function 1
+# Implement the function is_word_guessed that takes in two parameters a string, secret_word, and a list of letters (strings), letters_guessed.
+# This function returns a boolean, True if secret_word has been guessed and False otherwise.
 
 
 
 
 
-# Problem 2
-# Define a function codename_gen() that takes 3 parameters each of them strings: first name, last name, favorite word 
-# Returns a string codename starting with Mr.codename_here
-# Here is the rule for the codename generations:
-# Take the first and last letters of the first name, wrap them around the middle 3 letters of the last name, and add the length of the favorite word converted
-# Example call codename_gen("Natasha", "Romanoff", "Viper") -> Mr.Nanoa-5
-#              codename_gen("Alex", "Pierce", "Hydra") -> Mr.Aercx-5
-# Only the first letter should be capitalized 
-# Help for the last name middle characters: 
-# If the last name is odd length, take the middle character and the characters to the left and right 
-# If the last name is even lrngth, take the 2 middle characters and the character to the right 
-# Ex. Gomez -> ome, Buhr -> uhr, Miller -> lle
+# Helper Function 2
+# Implement the function get_guessed_word that takes in two parameters a string, secret_word, and a list of letters, letters_guessed. 
+# This function returns a string that is comprised of letters and underscores, based on what letters in letters_guessed are in secret_word.
 
 
 
 
 
+# Helper Function 3 
+# Implement the function get_available_letters that takes in one parameter a list of letters, letters_guessed.
+# This function returns a string that is comprised of lowercase English letters all lowercase English letters that are not in letters_guessed.
